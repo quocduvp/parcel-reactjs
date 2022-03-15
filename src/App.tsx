f694b9b610ef01ab13d833d5d9be73933dcfceb9
Dự NDQ <dundq@edoctor.vn>
2022-03-15 10:17:46 +0700

    Update version

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
 import * as React from "react";
-import "./App.css";
-import Messages from "./components/Messages";
 function App() {
-  const [isOpenChat, setOpenChat] = React.useState(false);
-
-  const handleOpenChat = (e: any) => {
-    e.preventDefault();
-    setOpenChat(true);
-  };
-
-  const handleCloseChat = (e: any) => {
-    e.preventDefault();
-    setOpenChat(false);
-  };
   return (
     <div>
-      <button
-        className="open-button"
-        style={{
-          width: "50px",
-          height: "50px",
-          borderRadius: "50%",
-          fontSize: "25px",
-        }}
-      >
-        <i
-          className="fa fa-comments-o"
-          aria-hidden="true"
-          onClick={handleOpenChat}
-        ></i>
-      </button>
-      {isOpenChat ? (
-        <Messages isOpenChat={isOpenChat} handleCloseChat={handleCloseChat} />
-      ) : (
-        ""
-      )}
+      <h1>Hello Parcel</h1>
     </div>
   );
 }
